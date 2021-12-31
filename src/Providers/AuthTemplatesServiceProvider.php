ac0733231b2ab561ede2e99ef66b86faefffb676
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2021-12-31 08:31:08 +0100

    Add email template for account must be enable by admin event

--- src/Providers/AuthTemplatesServiceProvider.php
+++ src/Providers/AuthTemplatesServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace EscolaLms\TemplatesEmail\Providers;
 
+use EscolaLms\Auth\Events\EscolaLmsAccountMustBeEnableByAdminTemplateEvent;
 use EscolaLms\Auth\Events\PasswordForgotten;
 use EscolaLms\Templates\Facades\Template;
 use EscolaLms\TemplatesEmail\Auth\ResetPasswordVariables;
 use EscolaLms\TemplatesEmail\Auth\VerifyEmailVariables;
+use EscolaLms\TemplatesEmail\Auth\VerifyUserAccountVariables;
 use EscolaLms\TemplatesEmail\Core\EmailChannel;
 use EscolaLms\TemplatesEmail\Events\Registered;
 use Illuminate\Support\ServiceProvider;
@@ -16,5 +18,6 @@ class AuthTemplatesServiceProvider extends ServiceProvider
     {
         Template::register(PasswordForgotten::class, EmailChannel::class, ResetPasswordVariables::class);
         Template::register(Registered::class, EmailChannel::class, VerifyEmailVariables::class);
+        Template::register(EscolaLmsAccountMustBeEnableByAdminTemplateEvent::class, EmailChannel::class, VerifyUserAccountVariables::class);
     }
 }
