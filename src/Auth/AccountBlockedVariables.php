737388ec8277ca716bcfbb2e9eca11fb5e0deb25
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-01-14 15:19:56 +0100

    Refactor

--- src/Auth/AccountBlockedVariables.php
+++ src/Auth/AccountBlockedVariables.php
@@ -2,50 +2,10 @@
 
 namespace EscolaLms\TemplatesEmail\Auth;
 
-use EscolaLms\Core\Models\User;
-use EscolaLms\Templates\Events\EventWrapper;
-use EscolaLms\TemplatesEmail\Core\EmailVariables;
 use Illuminate\Support\Facades\Lang;
 
-class AccountBlockedVariables extends EmailVariables
+class AccountBlockedVariables extends UserVariables
 {
-    const VAR_USER_EMAIL      = "@VarUserEmail";
-    const VAR_USER_FIRST_NAME = "@VarStudentFirstName";
-    const VAR_USER_LAST_NAME  = "@VarStudentLastName";
-    const VAR_USER_FULL_NAME  = "@VarStudentFullName";
-
-    public static function mockedVariables(?User $user = null): array
-    {
-        $faker = \Faker\Factory::create();
-        return array_merge(parent::mockedVariables($user), [
-            self::VAR_USER_EMAIL => $faker->email,
-            self::VAR_USER_FIRST_NAME => $faker->firstName,
-            self::VAR_USER_LAST_NAME => $faker->lastName,
-            self::VAR_USER_FULL_NAME => $faker->name,
-        ]);
-    }
-
-    public static function variablesFromEvent(EventWrapper $event): array
-    {
-        $user = $event->getUser();
-        return [
-            self::VAR_USER_EMAIL => $user->email,
-            self::VAR_USER_FIRST_NAME => $user->firstName,
-            self::VAR_USER_LAST_NAME => $user->lastName,
-            self::VAR_USER_FULL_NAME => $user->name,
-        ];
-    }
-
-    public static function requiredVariables(): array
-    {
-        return [];
-    }
-
-    public static function requiredVariablesInSection(string $sectionKey): array
-    {
-        return [];
-    }
-
     public static function defaultSectionsContent(): array
     {
         return [
@@ -63,10 +23,5 @@ class AccountBlockedVariables extends EmailVariables
                 . '</mj-text>'
             )
         ];
-    }
-
-    public static function assignableClass(): ?string
-    {
-        return null;
     }
 }
