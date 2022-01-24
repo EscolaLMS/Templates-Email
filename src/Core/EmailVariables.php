6e6680e7f9c4bb5fbe119eb72ff5e8aa62c9ce9a
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-01-24 14:11:45 +0100

    add global settings

--- src/Core/EmailVariables.php
+++ src/Core/EmailVariables.php
@@ -5,40 +5,21 @@ namespace EscolaLms\TemplatesEmail\Core;
 use EscolaLms\Core\Models\User;
 use EscolaLms\Templates\Contracts\TemplateVariableContract;
 use EscolaLms\Templates\Core\AbstractTemplateVariableClass;
+use EscolaLms\Templates\Core\SettingsVariables;
 use EscolaLms\Templates\Events\EventWrapper;
 use EscolaLms\TemplatesEmail\EscolaLmsTemplatesEmailServiceProvider;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 
 abstract class EmailVariables extends AbstractTemplateVariableClass implements TemplateVariableContract
 {
-    const VAR_APP_NAME  = '@VarAppName';
-    const VAR_LOGO      = '@VarLogo';
-
     public static function mockedVariables(?User $user = null): array
     {
-        return [
-            self::VAR_APP_NAME => config('app.name'),
-            self::VAR_LOGO => config('global.logo'),
-        ];
+        return SettingsVariables::getSettingsValues();
     }
 
     public static function variablesFromEvent(EventWrapper $event): array
     {
-        $logo = config('global.logo');
-        if (Storage::exists($logo)) {
-            $logo = Storage::url($logo);
-        }
-        else if (File::exists(public_path($logo))) {
-            $logo = URL::asset($logo);
-        }
-
-        return [
-            self::VAR_APP_NAME => config('app.name'),
-            self::VAR_LOGO => $logo
-        ];
+        return SettingsVariables::getSettingsValues();
     }
 
     public static function requiredSections(): array
