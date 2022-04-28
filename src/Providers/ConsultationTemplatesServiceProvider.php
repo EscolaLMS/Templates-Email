06c8cab869dc5f2c9061efd8e254ff8f7dba54ac
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-04-28 10:07:51 +0200

    Add variables to register temp

--- src/Providers/ConsultationTemplatesServiceProvider.php
+++ src/Providers/ConsultationTemplatesServiceProvider.php
@@ -9,7 +9,9 @@ use EscolaLms\Consultations\Events\RejectTermWithTrainer;
 use EscolaLms\Consultations\Events\ReminderAboutTerm;
 use EscolaLms\Consultations\Events\ReportTerm;
 use EscolaLms\TemplatesEmail\Consultations\ApprovedTermVariables;
+use EscolaLms\TemplatesEmail\Consultations\ApprovedTermWithTrainerVariables;
 use EscolaLms\TemplatesEmail\Consultations\RejectTermVariables;
+use EscolaLms\TemplatesEmail\Consultations\RejectTermWithTrainerVariables;
 use EscolaLms\TemplatesEmail\Consultations\ReminderAboutTermVariables;
 use EscolaLms\TemplatesEmail\Consultations\ReminderTrainerAboutTermVariables;
 use EscolaLms\TemplatesEmail\Consultations\ReminderTrainerAboutTerm;
@@ -22,9 +24,9 @@ class ConsultationTemplatesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Template::register(ApprovedTermWithTrainer::class, EmailChannel::class, ApprovedTermWithTrainer::class);
+        Template::register(ApprovedTermWithTrainer::class, EmailChannel::class, ApprovedTermWithTrainerVariables::class);
         Template::register(ApprovedTerm::class, EmailChannel::class, ApprovedTermVariables::class);
-        Template::register(RejectTermWithTrainer::class, EmailChannel::class, RejectTermWithTrainer::class);
+        Template::register(RejectTermWithTrainer::class, EmailChannel::class, RejectTermWithTrainerVariables::class);
         Template::register(RejectTerm::class, EmailChannel::class, RejectTermVariables::class);
         Template::register(ReportTerm::class, EmailChannel::class, ReportTermVariables::class);
         Template::register(ReminderAboutTerm::class, EmailChannel::class, ReminderAboutTermVariables::class);
