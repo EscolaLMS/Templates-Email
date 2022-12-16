4d9b6b61a718641aa5f668f503cc0305968160e0
Hubert Krzysztofiak <62691459+HerbertIV@users.noreply.github.com>
2022-12-16 11:51:37 +0100

    Feature/star 40 (#51)
    * Repair bug in templte email
    
    * Add similar fix to consultation variable
    
    Co-authored-by: Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>

--- src/Webinar/CommonWebinarVariables.php
+++ src/Webinar/CommonWebinarVariables.php
@@ -3,6 +3,7 @@
 namespace EscolaLms\TemplatesEmail\Webinar;
 
 use Carbon\Carbon;
+use DateTimeInterface;
 use EscolaLms\Core\Models\User;
 use EscolaLms\Templates\Events\EventWrapper;
 use EscolaLms\TemplatesEmail\Core\EmailVariables;
@@ -26,12 +27,20 @@ abstract class CommonWebinarVariables extends EmailVariables
 
     public static function variablesFromEvent(EventWrapper $event): array
     {
+        if ($event->getWebinar()->active_to instanceof DateTimeInterface) {
+            $proposedTerm = $event->getWebinar()->active_to;
+        } else {
+            $proposedTerm = Carbon::make($event->getWebinar()->active_to);
+        }
+
         return array_merge(parent::variablesFromEvent($event), [
             self::VAR_USER_NAME    => $event->getUser()->name,
             self::VAR_WEBINAR_TITLE => $event->getWebinar()->name,
-            self::VAR_WEBINAR_PROPOSED_TERM => Carbon::make($event->getWebinar()->active_to)
+            self::VAR_WEBINAR_PROPOSED_TERM => $proposedTerm ?
+                $proposedTerm
                 ->setTimezone($event->getUser()->current_timezone)
-                ->format('Y-m-d H:i:s'),
+                ->format('Y-m-d H:i:s') :
+                '',
         ]);
     }
 
