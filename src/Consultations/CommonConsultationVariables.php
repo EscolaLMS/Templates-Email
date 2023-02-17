3f34e573715b972f0357764469d65649c82c6de7
Witold Wiśniewski <58150098+daVitekPL@users.noreply.github.com>
2023-02-17 18:53:05 +0100

    Added variables for consultations date start and end (#57)
    * Added variables for consultations date start and end
    
    * Update composer

--- src/Consultations/CommonConsultationVariables.php
+++ src/Consultations/CommonConsultationVariables.php
@@ -4,6 +4,7 @@ namespace EscolaLms\TemplatesEmail\Consultations;
 
 use Carbon\Carbon;
 use EscolaLms\Consultations\Models\ConsultationUserPivot;
+use EscolaLms\Consultations\Services\Contracts\ConsultationServiceContract;
 use EscolaLms\Core\Models\User;
 use EscolaLms\Templates\Events\EventWrapper;
 use EscolaLms\TemplatesEmail\Core\EmailVariables;
@@ -13,14 +14,19 @@ abstract class CommonConsultationVariables extends EmailVariables
     const VAR_USER_NAME       = '@VarUserName';
     const VAR_CONSULTATION_TITLE    = '@VarConsultationTitle';
     const VAR_CONSULTATION_PROPOSED_TERM    = '@VarConsultationProposedTerm';
+    const VAR_CONSULTATION_DATE_TIME_START = '@VarConsultationDateTimeStart';
+    const VAR_CONSULTATION_DATE_TIME_END = '@VarConsultationDateTimeEnd';
 
     public static function mockedVariables(?User $user = null): array
     {
         $faker = \Faker\Factory::create();
+        $date = $faker->dateTime();
         return array_merge(parent::mockedVariables(), [
             self::VAR_USER_NAME       => $faker->name(),
             self::VAR_CONSULTATION_TITLE    => $faker->word(),
-            self::VAR_CONSULTATION_PROPOSED_TERM => $faker->dateTime()->format('Y-m-d H:i:s'),
+            self::VAR_CONSULTATION_PROPOSED_TERM => $date->format('Y-m-d H:i:s'),
+            self::VAR_CONSULTATION_DATE_TIME_START => Carbon::parse($date)->toISOString(),
+            self::VAR_CONSULTATION_DATE_TIME_END => Carbon::parse($date)->addHour()->toISOString(),
         ]);
     }
 
@@ -32,14 +38,18 @@ abstract class CommonConsultationVariables extends EmailVariables
                 $executedAt = Carbon::make($executedAt);
             }
             $executedAt = $executedAt
-                ->setTimezone($event->getUser()->current_timezone)
-                ->format('Y-m-d H:i:s');
+                ->setTimezone($event->getUser()->current_timezone);
+
         }
+        $executedTo = $executedAt ? app(ConsultationServiceContract::class)
+            ->generateDateTo($executedAt, $event->getConsultationTerm()->consultation->getDuration()) : $executedAt;
 
         return array_merge(parent::variablesFromEvent($event), [
             self::VAR_USER_NAME    => $event->getUser()->name,
             self::VAR_CONSULTATION_TITLE => $event->getConsultationTerm()->consultation->name,
-            self::VAR_CONSULTATION_PROPOSED_TERM => $executedAt,
+            self::VAR_CONSULTATION_PROPOSED_TERM => $executedAt->format('Y-m-d H:i:s'),
+            self::VAR_CONSULTATION_DATE_TIME_START => $executedAt->toISOString(),
+            self::VAR_CONSULTATION_DATE_TIME_END => $executedTo->toISOString(),
         ]);
     }
 
