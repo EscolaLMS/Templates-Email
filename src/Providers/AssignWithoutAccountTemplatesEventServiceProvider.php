8f3761e69cc06cc63e1f3a652383141203c18481
Dawid Miklas <58490533+dicani0@users.noreply.github.com>
2023-04-27 13:36:18 +0200

    emails for deleting user submissions (#73)
    Co-authored-by: dawid.miklas <dawid.miklas@escolasoft.com>

--- src/Providers/AssignWithoutAccountTemplatesEventServiceProvider.php
+++ src/Providers/AssignWithoutAccountTemplatesEventServiceProvider.php
@@ -4,9 +4,13 @@ namespace EscolaLms\TemplatesEmail\Providers;
 
 use EscolaLms\AssignWithoutAccount\Events\AssignToProduct;
 use EscolaLms\AssignWithoutAccount\Events\AssignToProductable;
+use EscolaLms\AssignWithoutAccount\Events\UnassignProduct;
+use EscolaLms\AssignWithoutAccount\Events\UnassignProductable;
 use EscolaLms\Templates\Facades\Template;
 use EscolaLms\TemplatesEmail\AssignWithoutAccount\AssignToProductableVariables;
 use EscolaLms\TemplatesEmail\AssignWithoutAccount\AssignToProductVariables;
+use EscolaLms\TemplatesEmail\AssignWithoutAccount\UnassignProductableVariables;
+use EscolaLms\TemplatesEmail\AssignWithoutAccount\UnassignProductVariables;
 use EscolaLms\TemplatesEmail\Core\EmailChannel;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
@@ -25,5 +29,16 @@ class AssignWithoutAccountTemplatesEventServiceProvider extends EventServiceProv
             EmailChannel::class,
             AssignToProductableVariables::class
         );
+
+        Template::register(
+            UnassignProduct::class,
+            EmailChannel::class,
+            UnassignProductVariables::class
+        );
+
+        Template::register(
+            UnassignProductable::class,
+            EmailChannel::class,
+            UnassignProductableVariables::class);
     }
 }
